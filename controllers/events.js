b7f1c8a6197e60fd6f0512126bfd2247c5b90a64
Franco Nicolas Pagano <141446501+Franco1812@users.noreply.github.com>
2024-09-09 15:15:12 -0300

    commit8/EventosDb

--- controllers/events.js
+++ controllers/events.js
@@ -1,39 +1,121 @@
 const response = require("express");
+ const Evento = require("../models/Evento");
 
-const getEventos = (req, res = response) => {
+const getEventos = async(req, res = response) => {
+  const eventos = await Evento.find()
+      .populate("user","name");
   res.json({
     ok: true,
-    msg: "obtener getEventos",
+    eventos
   });
 };
 
-const crearEventos = (req, res = response) => {
+const crearEventos = async(req, res = response) => {
   //verificar que tenga el evento
-      console.log(req.body);
+      const evento  = new Evento(req.body);
+      try {
+        evento.user = req.uid;
+        const eventoGuardado = await evento.save()
+      res.json({
+        ok:true,
+        evento: eventoGuardado
+      })
+      } catch (error) {
+          console.log(error);
+          res.status(500).json({
+            ok:false,
+            msg:"hable con el admin"
+          })
+      }
+  };
+
+
+  const actualizarEventos = async( req, res = response ) => {
+    
+    const eventoId = req.params.id;
+    const uid = req.uid;
+
+    try {
+
+        const evento = await Evento.findById( eventoId );
+
+        if ( !evento ) {
+            return res.status(404).json({
+                ok: false,
+                msg: 'Evento no existe por ese id'
+            });
+        }
+
+        if ( evento.user && evento.user.toString() !== uid ) {
+            return res.status(401).json({
+                ok: false,
+                msg: 'No tiene privilegio de editar este evento'
+            });
+        }
+
+        const nuevoEvento = {
+            ...req.body,
+            user: uid
+        }
+
+        const eventoActualizado = await Evento.findByIdAndUpdate( eventoId, nuevoEvento, { new: true } );
+
+        res.json({
+            ok: true,
+            evento: eventoActualizado
+        });
+
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Hable con el administrador'
+        });
+    }
+
+}
+
+const eliminarEventos = async( req, res = response ) => {
+
+  const eventoId = req.params.id;
+  const uid = req.uid;
+
+  try {
+
+      const evento = await Evento.findById( eventoId );
+
+      if ( !evento ) {
+          return res.status(404).json({
+              ok: false,
+              msg: 'Evento no existe por ese id'
+          });
+      }
+
+      if ( evento.user && evento.user.toString() !== uid ) {
+          return res.status(401).json({
+              ok: false,
+              msg: 'No tiene privilegio de eliminar este evento'
+          });
+      }
+
+
+      await Evento.findByIdAndDelete( eventoId );
+
+      res.json({ ok: true });
+
       
+  } catch (error) {
+      console.log(error);
+      res.status(500).json({
+          ok: false,
+          msg: 'Hable con el administrador'
+      });
+  }
+}
 
-    res.json({
-      ok: true,
-      msg: "obtener crearEventos",
-    });
-  };
 
-
-  const actualizarEventos = (req, res = response) => {
-    res.json({
-      ok: true,
-      msg: "obtener actualizarEventos",
-    });
-  };
-
-const eliminarEventos = (req, res = response) => {
-    res.json({
-      ok: true,
-      msg: "obtener eliminarEventos",
-    });
-  };
-
-  module.exports = {
+module.exports = {
     getEventos,
     crearEventos,
     actualizarEventos,
--- models/Evento.js
+++ models/Evento.js
@@ -1,27 +1,36 @@
-
-const {Schema, model } = require("mongoose");
+const { Schema, model } = require('mongoose');
 
 const EventoSchema = Schema({
 
     title: {
         type: String,
-        require: true,
+        required: true
     },
     notes: {
-        type: String,
+        type: String,        
     },
     start: {
         type: Date,
-        require: true
+        required: true
     },
     end: {
-        type: Date.now,
-        require: true
+        type: Date,
+        required: true
     },
     user: {
         type: Schema.Types.ObjectId,
-        ref: "Usuario"
+        ref: 'Usuario',
+        required: true
     }
+
 });
 
-module.exports =  model("Evento", EventoSchema);
+EventoSchema.method('toJSON', function() {
+    const { __v, _id, ...object } = this.toObject();
+    object.id = _id;
+    return object;
+});
+
+
+
+module.exports = model('Evento', EventoSchema );
